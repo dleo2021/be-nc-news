0b983f3c0677cb9ddac631ad3257dbcd642d3e69
dleo2021 <danleonard@hotmail.co.uk>
2024-07-15 13:57:39 +0100

    s

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const app = express();
 
 app.get("/api/topics", getTopics)
 
+app.all("*", (request, response) => {
+    response.status(404).send({message: "Path not found"})
+})
+
+app.use((err, request, response, next) => {
+    response.status(500).send({message: "Internal Server Error"})
+})
+
+module.exports = app
+const express = require("express");
+const { getTopics } = require("./controllers/controller");
+const app = express();
+
+app.get("/api/topics", getTopics)
+
 app.use((err, request, response, next) => {
     response.status(500).send({message: "Internal Server Error"})
 })
