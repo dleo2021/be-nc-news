03aa359014b634be3ad111c6dac430f64de86737
dleo2021 <danleonard@hotmail.co.uk>
2024-07-15 14:06:47 +0100

    Update error handling to deal with invalid urls

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,31 +1,42 @@
 const app = require("../app");
 const db = require("../db/connection");
 const request = require("supertest");
-const seed = require("../db/seeds/seed")
-const data = require("../db/data/test-data/index")
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
 
 beforeEach(() => {
-    return seed(data);
-  });
-  
-  afterAll(() => {
-    return db.end();
-  });
+  return seed(data);
+});
 
-  describe("/api/topics", () => {
-    it("GET 200: Responds with an array of topic objects with slug and description properties", () => {
-        return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({body}) => {
-            const topics = body.topics
-            expect(topics).toHaveLength(3)
-            topics.forEach((topic) => {
-                expect(topic).toEqual({
-                    slug: expect.any(String),
-                    description: expect.any(String)
-                })
-            })
-        })
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET /api/invalid-url", () => {
+  it("Should return a status 404 - not found", () => {
+    return request(app)
+    .get("/api/invalid-url")
+    .expect(404)
+    .then(({body}) => {
+      expect(body.message).toBe("Not found")
     })
   })
+})
+
+describe("/api/topics", () => {
+  it("GET 200: Responds with an array of topic objects with slug and description properties", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const topics = body.topics;
+        expect(topics).toHaveLength(3);
+        topics.forEach((topic) => {
+          expect(topic).toEqual({
+            slug: expect.any(String),
+            description: expect.any(String),
+          });
+        });
+      });
+  });
+});
